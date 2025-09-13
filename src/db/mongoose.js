3ad593445c116718435c9b4873043fa4c4364d4c
PASIYAv12 <pasidusampath280@gmail.com>
2025-09-13 20:47:27 +0530

    Delete src/db/mongoose.js

--- src/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const logger = require('pino')();
-
-async function connect(uri) {
-  await mongoose.connect(uri, { dbName: 'pasiyamd' });
-  logger.info('MongoDB connected');
-}
-
-module.exports = { connect, mongoose };
