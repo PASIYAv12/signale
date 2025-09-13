570e24c5bebfb873d09f028753d33066300c140a
PASIYAv12 <pasidusampath280@gmail.com>
2025-09-13 20:48:55 +0530

    Delete src directory

--- src/db/models/admin.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const AdminSchema = new Schema({
-  phone: String, // e.g. "94766359869"
-  name: String,
-  role: String
-});
-
-module.exports = model('Admin', AdminSchema);
