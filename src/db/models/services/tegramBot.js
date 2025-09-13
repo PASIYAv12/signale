f2c483ebb18119eaa86ef0bacead2a8582d0e7e1
PASIYAv12 <pasidusampath280@gmail.com>
2025-09-13 20:45:11 +0530

    Create tegramBot.js

---
+++ src/db/models/services/tegramBot.js
@@ -0,0 +1,33 @@
+const { Telegraf } = require('telegraf');
+const Signal = require('../models/Signal');
+const Admin = require('../models/Admin');
+const logger = require('pino')();
+
+function formatSignalMessage(signal) {
+  return `📡 *PASIYA-MD SIGNALE BOT* 📡\n\n*${signal.type}* - ${signal.symbol}\nEntry: ${signal.entry}\nSL: ${signal.sl}\nTP: ${signal.tp}\nWallet: ${signal.sizeType}\n\nPOWERED_BY PASIYA-MD`;
+}
+
+function initTelegram(token, sendToChatId) {
+  const bot = new Telegraf(token);
+
+  // basic commands for admin (register, list)
+  bot.command('start', (ctx) => ctx.reply('PASIYA-MD Signal Bot is online.'));
+
+  // For demo: send latest active signals to a chat id (group or channel)
+  async function sendSignal(signal) {
+    const msg = formatSignalMessage(signal);
+    // send markdown
+    await bot.telegram.sendMessage(sendToChatId, msg, { parse_mode: 'Markdown' });
+    logger.info('Telegram signal sent', { symbol: signal.symbol });
+  }
+
+  // Send TP hit alert
+  async function sendTPHit(signal) {
+    const msg = `POWERED_BUY PASIYA-MD SIGNALE BOT\n${signal.symbol} TP HIT ✅`;
+    await bot.telegram.sendMessage(sendToChatId, msg);
+  }
+
+  return { bot, sendSignal, sendTPHit };
+}
+
+module.exports = initTelegram;
